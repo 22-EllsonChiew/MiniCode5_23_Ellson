87491a9c2eeaa3a1a889ee1ee454cde16f6b674d
Itzuzume <ellson637@gmail.com>
2020-12-03 12:12:30 +0800

    MiniCode5A_23_Ellson

---
+++ Prefix_23/Assets/Scrpits/GameManager_23.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameManager_23 : MonoBehaviour
+{
+    public static GameManager_23 instance;
+    public GameObject addEnergyPreFab;
+    public GameObject minusEnergyRreFab;
+    public int numberOfSpawn;
+    public float LevelTime;
+
+    
+    public Text Timer;
+    float timer = 10.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        if (LevelTime > 0)
+        {
+            for (int i = 0; i < numberOfSpawn; i++)
+            {
+                Vector3 randomPos = new Vector3(Random.Range(-15, 15),
+                    0,
+                    Random.Range(-15, 15));
+
+                //Instantiate(addEnergyPreFab, randomPos, Quaternion.identity);
+
+                if (Random.Range(0, 2) < 1)
+                {
+                    Instantiate(addEnergyPreFab, randomPos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(minusEnergyRreFab, randomPos, Quaternion.identity);
+                }
+            }
+        }
+
+        Timer.GetComponent<Text>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if(LevelTime> 0)
+        {
+            LevelTime -= Time.deltaTime;
+           // print("LevelTime: " + FormatTime(LevelTime));
+            Timer.GetComponent<Text>().text = "Time: " + LevelTime.ToString("0");
+        }
+        else
+        {
+            LevelTime = 0;
+            //print("Times Up!");
+            Timer.GetComponent<Text>().text = "Ran Out Of Time";
+            
+        }
+        
+    }
+
+    public string FormatTime(float time)
+    {
+        int minutes = (int)time / 60;
+        int seconds = (int)time - 60 * minutes;
+        int milliseconds = (int)(1000 * (time - minutes * 60 - seconds));
+        return string.Format("{0:00:{1:00:{2.00}", minutes, seconds, milliseconds);
+    }
+
+
+}
---
+++ Prefix_23/Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    float energyCount = 0;
+
+    public Text EnergyCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+       
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Add"))
+        {
+            energyCount += 5;
+            Destroy(collision.gameObject);
+            EnergyCount.GetComponent<Text>().text = "Energy: " + energyCount.ToString();
+        }
+        else if(collision.gameObject.CompareTag("Minus"))
+        {
+            energyCount -= 5;
+            Destroy(collision.gameObject);
+            EnergyCount.GetComponent<Text>().text = "Energy: " + energyCount.ToString();
+        }
+    }
+
+
+
+}
